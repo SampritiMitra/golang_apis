3809a45e1a15f744bbb9f6405d328bc56c553444
SampritiMitra <sampriti.mitracd.eee14@itbhu.ac.in>
2019-08-20 12:50:47 +0530

    minor changes in concurrent

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,3 +1,184 @@
+//package controllers
+//import (
+//	"encoding/json"
+//	"fmt"
+//	"github.com/SampritiMitra/golang_apis/models"
+//	"math"
+//	"time"
+//	guuid "github.com/google/uuid"
+//	"io"
+//	"io/ioutil"
+//	"net/http"
+//	"os"
+//	//"time"
+//)
+//var M map[string]models.Response
+//var Stat map[string]string
+//var Count int
+//
+//func HomeLink(w http.ResponseWriter, r *http.Request) {
+//	w.WriteHeader(200)
+//	fmt.Fprintf(w, "OK")
+//}
+////func Download(w http.ResponseWriter, r *http.Request){
+////	var newLink models.Links
+////	reqBody, err := ioutil.ReadAll(r.Body)
+////	if err != nil {
+////		fmt.Fprintf(w, "Kindly enter proper data")
+////	}
+////	json.Unmarshal(reqBody, &newLink)
+////	models.Request = append(models.Request, newLink)
+////	w.WriteHeader(http.StatusCreated)
+////	start_time:=time.Now()
+////	fmt.Println(start_time)
+////	Stat=make(map[string]string)
+////	s:=guuid.New().String()
+////	M=make(map[string]models.Response)
+////	status:="Queued"
+////	M[s]=models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
+////	id:=&models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
+////	by,_:=json.Marshal(id)
+////	w.Write(by)
+////	if newLink.Types=="Serial"{
+////		Serial(newLink)
+////	}else{
+////		Concurrent(newLink)
+////	}
+////	end_time:=time.Now()
+////
+////	M[s] = models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
+////	id = &models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
+////
+////	by,_=json.Marshal(id)
+////	w.Write(by)
+////	fmt.Println(M[s].Start_time)
+////}
+//
+//func Download(w http.ResponseWriter, r *http.Request){
+//	var newLink models.Links
+//	reqBody, err := ioutil.ReadAll(r.Body)
+//	if err != nil {
+//		fmt.Fprintf(w, "Kindly enter proper data")
+//	}
+//	json.Unmarshal(reqBody, &newLink)
+//	models.Request = append(models.Request, newLink)
+//	w.WriteHeader(http.StatusCreated)
+//	start_time:=time.Now()
+//	//fmt.Println(start_time)
+//	Stat=make(map[string]string)
+//	s:=guuid.New().String()
+//	M=make(map[string]models.Response)
+//	status:="Queued"
+//	M[s]=models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
+//	id:=&models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
+//	by,_:=json.Marshal(id)
+//	w.Write(by)
+//	//var Serial_obj models.Serial
+//	//var Concurrent_obj models.Concurrent
+//
+//	if newLink.Types=="Serial"{
+//		//Serial_obj.Urls=newLink.Urls
+//		//Serial_obj.Downloader()
+//		Serial(newLink)
+//
+//	}else{
+//		//Concurrent_obj.Urls=newLink.Urls
+//		//Concurrent_obj.Downloader()
+//		Concurrent(newLink)
+//	}
+//	end_time:=time.Now()
+//
+//	M[s] = models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
+//	id = &models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
+//
+//	by,_=json.Marshal(id)
+//	w.Write(by)
+//	fmt.Println(M[s].Start_time)
+//}
+//func DownloadFile(filepath string, url string) error {
+//	// Get the data
+//	resp, err := http.Get(url)
+//	if err != nil {
+//		return err
+//	}
+//	defer resp.Body.Close()
+//
+//	// Create the file
+//	out, err := os.Create(filepath)
+//	if err != nil {
+//		return err
+//	}
+//	defer out.Close()
+//
+//	// Write the body to file
+//	_, err = io.Copy(out, resp.Body)
+//	return err
+//}
+//
+//func DF(filepath string, url string, Count *int) error {
+//	// Get the data
+//	resp, err := http.Get(url)
+//	*Count++
+//	if err != nil {
+//		return err
+//	}
+//	defer resp.Body.Close()
+//
+//	// Create the file
+//	out, err := os.Create(filepath)
+//	if err != nil {
+//		return err
+//	}
+//	defer out.Close()
+//
+//	// Write the body to file
+//	_, err = io.Copy(out, resp.Body)
+//	//if *Count==length{
+//	//	*status="Successful"
+//	//}
+//	return err
+//}
+//
+//func Status(w http.ResponseWriter, r *http.Request){
+//	var Id models.Id
+//	reqBody, err := ioutil.ReadAll(r.Body)
+//	if err != nil {
+//		fmt.Fprintf(w, "Kindly enter proper data")
+//	}
+//	json.Unmarshal(reqBody, &Id)
+//	res:=M[Id.Id]
+//	id:=&models.Response{Id:res.Id,Start_time:res.Start_time,End_time:res.End_time,Status:res.Status,Download_type:res.Download_type,Files:res.Files}
+//	by,_:=json.Marshal(id)
+//	w.Write(by)
+//	fmt.Println("status",res.Start_time)
+//}
+//
+//func Serial(newLink models.Links){
+//	for index,Url:=range newLink.Urls{
+//		Count++
+//		path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index)
+//		Stat[Url]=path
+//		if err := DownloadFile(path, Url); err != nil {
+//			panic(err)
+//		}
+//	}
+//}
+//
+//func Concurrent(newLink models.Links){
+//	Count=0
+//	simul:=5
+//	fmt.Println(len(newLink.Urls))
+//	for index:=0;index<len(newLink.Urls);index+=simul{
+//		for i:=0;i<int(math.Min(float64(simul),float64(len(newLink.Urls)-index)));i++{
+//			path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index+i)
+//			fmt.Println(path)
+//			Url:=newLink.Urls[index+i]
+//			Stat[Url]=path
+//			go DF(path, Url,&Count)
+//		}
+//	}
+//}
+
 package controllers
 
 import (
@@ -14,7 +195,6 @@ import (
 )
 var M map[string]models.Response
 var Stat map[string]string
-var count int
 
 func HomeLink(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
@@ -30,29 +210,21 @@ func Download(w http.ResponseWriter, r *http.Request){
 	models.Request = append(models.Request, newLink)
 	w.WriteHeader(http.StatusCreated)
 	start_time:=time.Now()
-	fmt.Println(start_time)
 	Stat=make(map[string]string)
 	s:=guuid.New().String()
 	M=make(map[string]models.Response)
-	status:="Queued"
-	M[s]=models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
-	id:=&models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:status,Download_type:newLink.Types,Files:Stat}
-	by,_:=json.Marshal(id)
-	w.Write(by)
+	M[s]=models.Response{Id:s,Start_time:start_time,End_time:start_time,Status:"Queued",Download_type:newLink.Types,Files:Stat}
+
 	if newLink.Types=="Serial"{
 		Serial(newLink)
 	}else{
 		Concurrent(newLink)
 	}
 	end_time:=time.Now()
-
-
-	M[s] = models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
-	id = &models.Response{Id: s, Start_time: start_time, End_time: end_time, Status: "Successful", Download_type: newLink.Types, Files: Stat}
-
-	by,_=json.Marshal(id)
+	M[s]=models.Response{End_time:end_time,Status:"Successful",Files:Stat}
+	id:=&models.Response{Id:s,Start_time:start_time,End_time:end_time,Status:"Successful",Download_type:newLink.Types,Files:Stat}
+	by,_:=json.Marshal(id)
 	w.Write(by)
-	fmt.Println(M[s].Start_time)
 }
 func DownloadFile(filepath string, url string) error {
 	// Get the data
@@ -75,7 +247,6 @@ func DownloadFile(filepath string, url string) error {
 }
 func Serial(newLink models.Links){
 	for index,Url:=range newLink.Urls{
-		count++
 		path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index)
 		Stat[Url]=path
 		if err := DownloadFile(path, Url); err != nil {
@@ -83,7 +254,7 @@ func Serial(newLink models.Links){
 		}
 	}
 }
-func DF(filepath string, url string, count *int) error {
+func DF(filepath string, url string, count *int, ch chan string) error {
 	// Get the data
 	resp, err := http.Get(url)
 	*count++
@@ -101,23 +272,44 @@ func DF(filepath string, url string, count *int) error {
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
-	//if *count==length{
-	//	*status="Successful"
-	//}
+	ch<-"done"
 	return err
 }
 func Concurrent(newLink models.Links){
-	count=0
+	count:=0
 	simul:=5
 	fmt.Println(len(newLink.Urls))
 	for index:=0;index<len(newLink.Urls);index+=simul{
+		ch:=make(chan string,2*simul)
 		for i:=0;i<int(math.Min(float64(simul),float64(len(newLink.Urls)-index)));i++{
 			path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index+i)
-			fmt.Println(path)
 			Url:=newLink.Urls[index+i]
 			Stat[Url]=path
-			go DF(path, Url,&count)
+			go DF(path, Url,&count,ch)
 		}
+		go func() {
+			for{
+				y:=0
+				select{
+				case <-ch:
+					if(count==int(math.Min(float64(index+simul),float64(len(newLink.Urls))))){
+						if(count==len(newLink.Urls)){
+							return
+						}
+						// want to break outer if i has reached index+simul value
+						//simul is number of go routines simultaneously spawning
+						// like i goes from 0 to 5 and then break at 5
+						// ch close was not working
+						y=1
+						break
+					}
+				}
+				if y==1{
+					break
+				}
+			}
+		}()
+		fmt.Println("count",count)
 	}
 }
 func Status(w http.ResponseWriter, r *http.Request){
@@ -127,9 +319,5 @@ func Status(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Kindly enter proper data")
 	}
 	json.Unmarshal(reqBody, &Id)
-	res:=M[Id.Id]
-	id:=&models.Response{Id:res.Id,Start_time:res.Start_time,End_time:res.End_time,Status:res.Status,Download_type:res.Download_type,Files:res.Files}
-	by,_:=json.Marshal(id)
-	w.Write(by)
-	fmt.Println("status",res.Start_time)
+	fmt.Fprint(w,M[Id.Id])
 }
--- models/structs.go
+++ models/structs.go
@@ -1,3 +1,65 @@
+//package models
+//
+//import (
+//	"fmt"
+//	"github.com/SampritiMitra/golang_apis/controllers"
+//	"math"
+//	"time"
+//)
+//
+//type Downloading interface{
+//	Downloader()
+//}
+//type Links struct {
+//	Types          string    `json:"types"`
+//	Urls       	[]string `json:"urls"`
+//}
+//type Response struct {
+//	Id          string    `json:"id"`
+//	Start_time  time.Time     `json:"start_time"`
+//	End_time 	time.Time		`json:"end_time"`
+//	Status		string		`json:"status"`
+//	Download_type string	`json:"download_type"`
+//	Files		map[string]string	`json:"files"`
+//}
+//type Id struct{
+//	Id string `json:"id"`
+//}
+//type Serial struct{
+//	Urls[] string `json:"urls"`
+//}
+//type Concurrent struct{
+//	Urls[] string `json:"urls"`
+//}
+//
+//type AllLinks []Links
+//var Request = AllLinks{}
+//
+//func (newLink Serial) Downloader(){
+//	for index,Url:=range newLink.Urls{
+//		controllers.Count++
+//		path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index)
+//		controllers.Stat[Url]=path
+//		if err := controllers.DownloadFile(path, Url); err != nil {
+//			panic(err)
+//		}
+//	}
+//}
+//
+//func (newLink Concurrent) Downloader(){
+//	controllers.Count=0
+//	simul:=5
+//	fmt.Println(len(newLink.Urls))
+//	for index:=0;index<len(newLink.Urls);index+=simul{
+//		for i:=0;i<int(math.Min(float64(simul),float64(len(newLink.Urls)-index)));i++{
+//			path:=fmt.Sprintf("/users/sampritimitra/Desktop/file%d.jpg",index+i)
+//			fmt.Println(path)
+//			Url:=newLink.Urls[index+i]
+//			controllers.Stat[Url]=path
+//			go controllers.DF(path, Url,&controllers.Count)
+//		}
+//	}
+//}
 package models
 
 import "time"
